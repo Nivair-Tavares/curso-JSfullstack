ac64d7598f9b647053439de023b631fb9c58b7a0
Nivair <avindsn@gmail.com>
2025-07-14 14:44:15 +0100

    Primeiro commit: adiciona ola.js

---
+++ ola.js
@@ -0,0 +1,8 @@
+const nome = "Nivair";
+const idade = 25;
+console.log(`Olá, ${nome}!`);
+console.log(`Tens ${idade} anos.`);
+
+const anoAtual = 2025
+const anoNascimento = anoAtual - idade
+console.log(`Nasceste em: ${anoNascimento}`)
