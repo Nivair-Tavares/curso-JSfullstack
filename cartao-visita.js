755702cb699ad31d52dfeb340b9371cee897c64a
Nivair <avindsn@gmail.com>
2025-07-14 14:52:40 +0100

    Move ola.html para docs/index.html para usar Github Pages

---
+++ cartao-visita.js
@@ -0,0 +1,9 @@
+const nome = "Nivair"
+const idade = 25
+const profissao = "Designer Grafico"
+
+console.log("----------------------\n")
+console.log(`Nome: ${nome}\n`)
+console.log(`idade: ${idade}\n`)
+console.log(`Profissão: ${profissao}\n`)
+console.log("----------------------\n")
