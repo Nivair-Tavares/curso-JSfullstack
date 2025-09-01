334cd39cca914051edb9c90f009343251693e1cd
Neil.dev <avindsn@gmail.com>
2025-09-01 12:36:06 +0100

    Delete Módulo 1 directory

--- Módulo 1/cartao-visita.js
+++
@@ -1,9 +0,0 @@
-const nome = "Nivair"
-const idade = 25
-const profissao = "Designer Gráfico"
-
-console.log("----------------------\n")
-console.log(`Nome: ${nome}\n`)
-console.log(`Idade: ${idade}\n`)
-console.log(`Profissão: ${profissao}\n`)
-console.log("----------------------\n")
